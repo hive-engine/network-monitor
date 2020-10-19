9ba7a66395ec0d7fee721f68b404ca8f33b3d5c5
cryptomancer <8011332+bt-cryptomancer@users.noreply.github.com>
2020-10-19 02:22:30 +0000

    added block comparisons and error handling

--- monitor.js
+++ monitor.js
@@ -2,40 +2,74 @@ const nodeCleanup = require('node-cleanup');
 const fs = require('fs-extra');
 const SSC = require('sscjs');
 const config = require('./config');
+const nodemailer = require("nodemailer");
 
 let { lastSSCBlockParsed } = config; // eslint-disable-line prefer-const
 let sscNodes = [];
 
-config.nodes.forEach(node => {
-  sscNodes.push(new SSC(node));
-  console.log(`connected to Engine node: ${node}`);
-});
-
-async function fetchBlocks(blockNumber) {
-  const blocks = [];
-
-  try {
-    for (let i = 0; i < sscNodes.length; i += 1) {
-      const block = await sscNodes[i].getBlockInfo(blockNumber);
-      blocks.push(block);
-      console.log(`block[${blockNumber}] from ${config.nodes[i]}:`);
-      console.log(`hash: ${block.hash}`);
-      console.log('\n');
-    }
-  } catch (error) {
-    console.log(error);
-  }
-
-  return blocks;
+function initNodes() {
+  sscNodes = [];
+  config.nodes.forEach(node => {
+    sscNodes.push(new SSC(node));
+    console.log(`using Engine node: ${node}`);
+  });
 }
 
-const nodeBlocks = fetchBlocks(lastSSCBlockParsed);
+function allBlocksReceived(blocks) {
+  for (let k = 0; k < blocks.length; k += 1) {
+    if (!blocks[k]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+async function process(blockNumber) {
+  const blocks = [];
+  let newBlockNumber = blockNumber;
+
+  // fetch blocks from API
+  for (let i = 0; i < sscNodes.length; i += 1) {
+    try {
+      const block = await sscNodes[i].getBlockInfo(blockNumber);
+      blocks.push(block);
+    } catch (error) {
+      console.log(`error fetching block ${blockNumber} from ${config.nodes[i]}`);
+      console.log(error);
+      initNodes();
+      setTimeout(() => process(blockNumber), config.pollingTime);
+      return;
+    }
+  }
+
+  // do block comparions
+  if (allBlocksReceived(blocks)) {
+    let checksPassed = true;
+    for (let i = 0; i < blocks.length; i += 1) {
+      for (let j = 0; j < blocks.length; j += 1) {
+        if (JSON.stringify(blocks[i]) !== JSON.stringify(blocks[j])) {
+          console.log(`ERROR: block ${blockNumber} is different on ${config.nodes[i]} vs ${config.nodes[j]}`);
+          // TODO: figure out how to send e-mail alert here
+        }
+      }
+    }
+    if (checksPassed) {
+      console.log(`processed block ${blockNumber}`);
+    }
+    lastSSCBlockParsed = blockNumber;
+    newBlockNumber += 1;
+  }
+  setTimeout(() => process(newBlockNumber), config.pollingTime);
+}
+
+initNodes();
+process(lastSSCBlockParsed);
 
 // graceful app closing
 nodeCleanup((exitCode, signal) => { // eslint-disable-line no-unused-vars
   console.log('start saving conf'); // eslint-disable-line no-console
   const conf = fs.readJSONSync('./config.json');
-  conf.lastSSCBlockParsed = lastSSCBlockParsed;
+  conf.lastSSCBlockParsed = lastSSCBlockParsed + 1;
   fs.writeJSONSync('./config.json', conf, { spaces: 4 });
   console.log('done saving conf'); // eslint-disable-line no-console
 });
